66f7a3d8716f8c21cc1bd113bd133665fe68b794
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-06-01 12:51:33 +0300

    build

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use(express.json())
 app.use(express.static('build'))
 
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 let notes = [
     {
       id: 1,
