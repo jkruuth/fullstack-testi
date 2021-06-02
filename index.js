296fff4ab7285c584c4adaf674e51c45ec916775
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-06-02 16:43:52 +0300

    valivaihe

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const { request, response } = require('express')
 const express = require('express')
 const cors = require('cors')
-const app = express()
+const Note = require('./models/note')
 
+
+const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
@@ -17,6 +20,8 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
+
+
 
 let notes = [
     {
@@ -38,23 +43,25 @@ let notes = [
       important: true
     }
   ]
+
+  app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
+  })
+
   app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
   })
   
-  app.get('/api/notes', (req, res) => {
+  /* app.get('/api/notes', (req, res) => {
     res.json(notes)
-  })
+  }) */
   
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+      Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+      })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -74,7 +81,7 @@ let notes = [
   app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
@@ -84,15 +91,14 @@ let notes = [
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
     }
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
---
+++ mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+`mongodb+srv://joonasfs:${password}@cluster0.tm1cz.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Callback-functions suck',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(response => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
