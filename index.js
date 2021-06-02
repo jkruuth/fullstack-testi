9a4384a3313aeddf497f0a356c2a706d77225168
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-06-02 20:03:19 +0300

    Muut operaatiot

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const Note = require('./models/note')
 
-
 const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
 
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -21,8 +20,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-
-
 
 /* let notes = [
     {
@@ -59,17 +56,22 @@ app.use(requestLogger)
     res.json(notes)
   }) */
   
-  app.get('/api/notes/:id', (request, response) => {
+  app.get('/api/notes/:id', (request, response, next) => {
       Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
       })
+      .catch(error => next(error))
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-      const id = Number(request.params.id)
-      notes = notes.filter(note => note.id !== id)
-
-      response.status(204).end()
+  app.delete('/api/notes/:id', (request, response, next) => {
+      Note.findByIdAndRemove(request.params.id).then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
   const generateId = () => {
@@ -96,6 +98,42 @@ app.use(requestLogger)
       response.json(savedNote)
     })
   })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
+  const unknownEndpoint = (request, response) => {
+    response.status.send({ error: 'Unknown endpoint' })
+  }
+
+  // olemattomien osoitteiden käsittely
+  app.use(unknownEndpoint)
+
+
+  // virheellisten pyyntöjen käsittely
+  const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id '})
+    }
+
+    next(error)
+  }
+
+  app.use(errorHandler)
 
   const PORT = process.env.PORT
   app.listen(PORT, () => {
