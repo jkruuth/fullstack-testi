462ea2afd2abf61a95f74cb17991b4558ecf20f2
jkruuth <joonas.k.ruuth@student.jyu.fi>
2021-06-03 13:55:42 +0300

    tietokanta versio tuotanto

--- index.js
+++ index.js
@@ -81,12 +81,8 @@ app.use(requestLogger)
     return maxId + 1
   }
 
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
-  
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
   
     const note = new Note({
       content: body.content,
@@ -94,9 +90,12 @@ app.use(requestLogger)
       date: new Date(),
     })
   
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => savedNote.toJSON())
+        .then(savedAndFormattedNote => {
+          response.json(savedAndFormattedNote)
+        })
+        .catch(error => next(error))
   })
 
   app.put('/api/notes/:id', (request, response, next) => {
@@ -128,6 +127,8 @@ app.use(requestLogger)
 
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id '})
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
     }
 
     next(error)
